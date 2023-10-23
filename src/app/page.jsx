c059e23ce7604ea7d91c5a9108acf2da6f09fed6
jeet kanodia <jeetkanodia@gmail.com>
2023-10-23 21:05:09 +0530

    UI: contact and about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "public/hero.png";
 import styles from "./page.module.css";
+import Button from "./components/Button/Button";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,7 +13,7 @@ export default function Home() {
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry
         </p>
-        <button className={styles.button}>See Our Works</button>
+        <Button url="/portfolio" text={"See Our Works"} />
       </div>
       <div className={styles.item}>
         <Image src={Hero} className={styles.img} />
